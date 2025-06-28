ce38d9f1630ad2bcc38e5f0cbfff361a19b10f0c
james691curry <james691curry@gmail.com>
2025-06-29 00:00:46 +0100

    Fix case-sensitive import paths

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 // routes/auth.js
-const express = require('express');
+const express = require(‘express’);
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login } = require(’../controllers/authcontroller’);
 
 // @route   POST /api/auth/register
-router.post('/register', register);
+router.post(’/register’, register);
 
 // @route   POST /api/auth/login
-router.post('/login', login);
+router.post(’/login’, login);
 
 module.exports = router;
