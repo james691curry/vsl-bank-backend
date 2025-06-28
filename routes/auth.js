2c20f6f7409a919761c6c0a06bf5a4b7e2327ac4
james691curry <james691curry@gmail.com>
2025-06-29 00:40:07 +0100

    Fix: Correct case in authController import

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 // routes/auth.js
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authcontroller');
+const { register, login } = require('../controllers/authController');
 
 // @route   POST /api/auth/register
 router.post('/register', register);
